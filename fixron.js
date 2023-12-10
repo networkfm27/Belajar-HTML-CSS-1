50699bd2668abb0deca052dd4473e1ba1274e4c4
Fikron Maulana <fixron.maulana@gmail.com>
2023-12-10 14:32:32 +0700

    first commit

---
+++ fixron.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Lakukan sesuatu dengan data formulir, seperti mengirim ke server atau menampilkan pesan sukses.
+    alert("Pesan telah dikirim!");
+  });
